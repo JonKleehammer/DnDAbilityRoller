56634a4b61c3bbfbbaf58f1f2b50d85ab96ca3b0
Jon Kleehammer <jjk0102@unt.edu>
2019-09-07 14:41:00 -0500

    setting up prompt to take input, not actually taking input yet

--- AbilityRoller.py
+++ AbilityRoller.py
@@ -17,6 +17,11 @@ wis = {'animal handling', 'insight', 'medicine', 'perception', 'survival'}
 # charisma skills
 cha = {'deception', 'intimidation', 'performance', 'persuasion'}
 
-#putting all the abilities and their skills into an array of abilities
+# putting all the abilities and their skills into an array of abilities
 abilities = {str, dex, con, int, wis, cha}
 
+# Getting input from user? Just press enter to use default
+# TODO: implement this, leaving it out for faster testing
+print("How many saving throws and skill checks would you like? (default: 1 3)")
+saveCount = 1
+skillCount = 3
