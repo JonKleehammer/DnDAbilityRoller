69fb7cbc25b49c730328fb582cc86d84e5443178
Jon Kleehammer <jjk0102@unt.edu>
2019-09-07 14:17:01 -0500

    Setting up file

---
+++ AbilityRoller.py
@@ -0,0 +1,5 @@
+# Program by Jonathan Kleehammer
+# September 7th 2019
+# Program is designed to randomly choose abilities for skill checks and saving throws
+# The idea is that I feel like I was using a lot of similar skills while ignoring others
+# By randomly choosing skills I try to work them into my sessions so that all skills are represented
