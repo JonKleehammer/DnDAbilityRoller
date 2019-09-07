a391e23ff5356e3c3f108c6855d8377bfe3213b5
Jon Kleehammer <jjk0102@unt.edu>
2019-09-07 14:36:16 -0500

    set up abilities with their skills

--- AbilityRoller.py
+++ AbilityRoller.py
@@ -3,3 +3,17 @@
 # Program is designed to randomly choose abilities for skill checks and saving throws
 # The idea is that I feel like I was using a lot of similar skills while ignoring others
 # By randomly choosing skills I try to work them into my sessions so that all skills are represented
+
+# strength skills
+str = {'athletics'}
+# dexterity skills
+dex = {'acrobatics', 'sleight of hand', 'stealth'}
+# constitution "skills"
+con = {}
+# intelligence skills
+int = {'arcana', 'history', 'investigation', 'nature', 'religion'}
+# wisdom skills
+wis = {'animal handling', 'insight', 'medicine', 'perception', 'survival'}
+# charisma skills
+cha = {'deception', 'intimidation', 'performance', 'persuasion'}
+
